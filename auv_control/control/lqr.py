fix lqr with true rotation matrix

auv_control/control/lqr.py
@@ -30,7 +30,7 @@ class LQR:
                 [-20.64, 18.23, -0.21],
                 [-20.64, -18.23, -0.21]
             ]) / 100
-            self.J = np.eye(3) * 1.0
+            self.J = np.eye(3) * 1
         else:
             raise ValueError("Unknown robot type. Use 'HoveringAUV' or 'BlueROV2'.")
         self.rho = 997
@@ -80,7 +80,7 @@ class LQR:
         # self.Q[9:12] = 10  # angular velocity
         self.Q[0:3] = l_p  # position
         self.Q[3:6] = l_v  # velocity
-        self.Q[6:9] = [.01, .01, 0.02]  # 0.01 # rotation
+        self.Q[6:9] = [.01, .01, .02]  # 0.01 # rotation
         self.Q[9:12] = 0.01  # angular velocity
         self.Q = np.diag(self.Q)
 
@@ -101,11 +101,13 @@ class LQR:
         e = x.vec - x_d.vec
         u_til = -self.K @ e
 
-        # Feedback linearization
-        u_til[:3] = x.mat[:3, :3].T @ u_til[:3]  # rotate force to body framed
-        u_til[3:] += np.cross(x.mat[:3, :3].T @ np.array([0, 0, 1]),
-                              self.cob) * self.V * self.rho * self.gravity  # subtract off bouyant torque
+        # u_til[1:3] = -u_til[1:3]
+        # u_til[4:6] = -u_til[4:6]
 
+        # Feedback linearization
+        u_til[:3] = (x.mat[:3, :3] @ x.T_ned_to_nwu).T @ u_til[:3]  # rotate force to body framed
+        u_til[3:] += np.cross((x.mat[:3, :3]@ x.T_ned_to_nwu).T @ np.array([0, 0, 1]),
+                              self.cob) * self.V * self.rho * self.gravity  # subtract off bouyant torque
         # Convert forces/torques to thruster commands
         f = self.Minv @ u_til
 

auv_control/state.py
@@ -6,6 +6,7 @@ from inekf import SE3
 class State:
     """A uniform representation of our state from various sources.
 
+    Be careful!!!: PoseSensor、VelocitySensor must be in IMUSocket for NED coordinate system.
     Can come from a dictionary (HoloOcean), SE[2,6] object (InEKF library), or from a simple
     numpy array.     
 
@@ -16,12 +17,14 @@ class State:
         self.vec = np.zeros(12)
         self.mat = np.eye(5)
         self.bias = np.zeros(6)
+        self.T_ned_to_nwu = np.diag([1, -1, -1])
 
         # True State
         if isinstance(state, dict):
             self.vec[0:3] = state["PoseSensor"][:3, 3]
             self.vec[3:6] = state["VelocitySensor"]
-            self.vec[6:9] = rot_to_rpy(state["PoseSensor"][:3, :3])
+            self.vec[6:9] = rot_to_rpy(state["PoseSensor"][:3, :3] @ self.T_ned_to_nwu)
+            # self.vec[6:9] = rot_to_rpy(state["PoseSensor"][:3, :3])
             self.vec[9:12] = state["IMUSensorClean"][1]
 
             self.bias[0:3] = state["IMUSensor"][3]
@@ -70,3 +73,4 @@ class State:
 
 def rot_to_rpy(mat):
     return Rotation.from_matrix(mat).as_euler("xyz") * 180 / np.pi
+

auv_env/envs/agent.py
@@ -43,7 +43,7 @@ class AgentAuv(Agent):
                 l_p = np.random.normal(40, 15)
             else:
                 l_p = 50
-            self.controller = LQR(l_p=l_p)
+            self.controller = LQR(l_p=l_p, robo_type=robo_type)
         elif self.config['agent']['controller'] == "PID":
             self.controller = PID(robo_type=robo_type)
         else:

auv_env/envs/base.py
@@ -105,10 +105,16 @@ class WorldBase:
         self.obstacles = Obstacle(self.ocean, self.fix_depth, self.config)
 
         self.sensors = {}
+        if self.config['agent']['controller'] == 'LQR':
+            self.controller = 'LQR'
+        elif self.config['agent']['controller'] == 'PID':
+            self.controller = 'PID'
+        self.frequency = self.config['agent']['controller_config'][self.controller]['control_frequency']
+        
         self.set_limits()
 
     def step(self, action):
-        if self.action_dim == self.config['agent']['controller_config']['LQR']['action_dim']:
+        if self.controller == 'LQR':
             # waypoint for LQR
             global_waypoint = np.zeros(3)
             observed = []
@@ -119,16 +125,14 @@ class WorldBase:
             angle = action[2] * self.action_range_scale[2]
             global_waypoint[2] = self.agent.est_state.vec[8] + np.rad2deg(angle)
             self.action = global_waypoint
-            frequency = self.config['agent']['controller_config']['LQR']['control_frequency']
-        elif self.action_dim == self.config['agent']['controller_config']['PID']['action_dim']:
+        elif self.controller == 'PID':
             # cmd_vel for PID
             cmd_vel = CmdVel()
             cmd_vel.linear.x = action[0] * self.action_range_scale[0]
             cmd_vel.angular.z = action[1] * self.action_range_scale[1]
             self.action = cmd_vel
-            frequency = self.config['agent']['controller_config']['PID']['control_frequency']
 
-        for _ in range(frequency):
+        for _ in range(self.frequency):
             for i in range(self.num_targets):
                 target = 'target'+str(i)
                 if self.has_discovered[i]:

tests/test_keyboard.py
@@ -61,9 +61,10 @@ class KeyBoardCmd:
 
 from PIL import Image
 from torchvision import transforms
+from auv_control import scenario
 
-scenario = "SimpleUnderwater-Bluerov2" # "AUV_RGB"
-with holoocean.make(scenario) as env:
+# scenario = "SimpleUnderwater-Bluerov2" # "AUV_RGB"
+with holoocean.make(scenario_cfg=scenario) as env:
     kb = KeyBoardCmd(force=25)
     from auv_control import State
     for _ in range(20000):
@@ -76,7 +77,11 @@ with holoocean.make(scenario) as env:
         state = env.tick()
         state = state["auv0"]
         true_state = State(state)
-        print(true_state.vec[8])
+        # print(true_state.vec)
+        # print(state['VelocitySensor'])
+        state["PoseSensor"][:3, 3]
+        # t = np.diag([1, -1, -1])
+        # print(state["PoseSensorNED"][:3, :3] @ t)
         # if "LeftCamera" in state:
         #     pixels = state["LeftCamera"]
         #     cv2.namedWindow("Camera Output")

tests/test_lqr.py
@@ -18,7 +18,7 @@ ts = 1 / scenario["ticks_per_sec"]
 # num_ticks = int(num_seconds / ts)
 
 # Set everything up
-controller = LQR()
+controller = LQR(l_p=100)
 # observer = InEKF()
 # if route == "rrt":
 #     planner = RRT(num_seconds)
@@ -69,3 +69,4 @@ for i in range(20000):
     des_state = State(np.array([pos[0], pos[1], pos[2], 0.00, 0.00, 0.00, 0.00, 0.00, yaw, - 0.00, - 0.00, 0.00]))
     # des_state.vec = [-5, 3, -5, 0.00, 0.00, 0.00, 0.00, 0.00, 0.00, - 0.00, - 0.00, 0.00]
     u = controller.u(true_state, des_state)
+    print(max(u))

tests/test_lqr_1.py
@@ -1,3 +1,10 @@
+import sys
+import os
+
+ROOT_DIR = os.path.dirname(os.path.dirname(__file__))
+sys.path.append(ROOT_DIR)
+os.chdir(ROOT_DIR)
+
 import numpy as np
 import holoocean
 
@@ -11,7 +18,7 @@ ts = 1 / scenario["ticks_per_sec"]
 # num_ticks = int(num_seconds / ts)
 
 # Set everything up
-controller = LQR()
+controller = LQR(l_p=50, robo_type="BlueROV2")
 # observer = InEKF()
 # if route == "rrt":
 #     planner = RRT(num_seconds)
@@ -24,7 +31,7 @@ controller = LQR()
 
 # Run simulation!
 u = np.zeros(8)
-env = holoocean.make(scenario_cfg=scenario)
+env = holoocean.make("SimpleUnderwater-Bluerov2")
 # planner.draw_traj(env, num_seconds)
 
 # for i in tqdm(range(num_ticks)):
@@ -51,16 +58,28 @@ for i in range(20000):
 
     # Autopilot Commands
     depth = -5
+    # if t < 3000:
+    #     vel = [5 ,0, 0]
+    #     w = 0.5
+    #     yaw = 0
+    # # elif 3 <= t < 6:
+    # #     vel = [0,1,0]
+    # #     yaw = 0
+    # # elif 6 <= t < 9:
+    # #     vel = [0,0,1]
+    # #     yaw = 0
+    # des_state = State(np.array([0.00, 0.00, depth, vel[0], vel[1], vel[2], 0.00, 0.00, yaw, - 0.00, - 0.00, w]))
+    # # des_state.vec = [-5, 3, -5, 0.00, 0.00, 0.00, 0.00, 0.00, 0.00, - 0.00, - 0.00, 0.00]
+    # u = controller.u(true_state, des_state)
+
     if t < 3000:
-        vel = [5 ,0, 0]
-        w = 0.5
-        yaw = 0
-    # elif 3 <= t < 6:
-    #     vel = [0,1,0]
-    #     yaw = 0
-    # elif 6 <= t < 9:
-    #     vel = [0,0,1]
-    #     yaw = 0
-    des_state = State(np.array([0.00, 0.00, depth, vel[0], vel[1], vel[2], 0.00, 0.00, yaw, - 0.00, - 0.00, w]))
-    # des_state.vec = [-5, 3, -5, 0.00, 0.00, 0.00, 0.00, 0.00, 0.00, - 0.00, - 0.00, 0.00]
+        pos = [-5, 3, -5]
+        yaw = 90
+    elif 3 <= t < 6:
+        pos = [5, 3, -5]
+        yaw = 90
+    elif 6 <= t < 9:
+        pos = [5, -3, -8]
+        yaw = -90
+    des_state = State(np.array([pos[0], pos[1], pos[2], 0.00, 0.00, 0.00, 0.0, 0.00, yaw, - 0.00, - 0.00, 0.00]))
     u = controller.u(true_state, des_state)